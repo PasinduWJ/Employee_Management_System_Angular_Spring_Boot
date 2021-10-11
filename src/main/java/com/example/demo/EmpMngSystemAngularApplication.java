27348b034e4f50fea30892598759d16b02f3ec4f
PasinduWJ <pasindujayasekara3@gmail.com>
2021-10-11 15:25:16 +0530

    first commit

---
+++ src/main/java/com/example/demo/EmpMngSystemAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMngSystemAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMngSystemAngularApplication.class, args);
+	}
+
+}
